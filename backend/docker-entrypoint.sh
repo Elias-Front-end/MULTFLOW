84b5aa5f773cd4c5919dec0c876cd3970a7ffa12
fix: ELIAS <115664689+Elias-Front-end@users.noreply.github.com>
2026-01-18 10:24:52 -0300

    g

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Certifique-se de que este script seja executável (chmod +x) antes de usá-lo no Dockerfile
-# Inicie o aplicativo PM2 com as opções desejadas
-# pm2-docker start pm2.config.js
-pm2-docker start ./dist/server.js
+# Execute database migrations
+npx sequelize db:migrate
+
+# Start the application using pm2-runtime
+pm2-runtime start ecosystem.config.js
